eac140c49d455fc3142d69e31fc93b72eb409cf2
olasgolas11 <olekgolus02@gmail.com>
2023-05-22 16:20:26 +0200

    Clear move function, extract to seperate functions
    Clear more code

--- Player.py
+++ Player.py
@@ -18,108 +18,64 @@ class MoveState(Enum):
 class Player:
     playerDirection = Direction.DOWN
     playerMoveState = MoveState.STANDING
-    playerMoveFrame = None
+    playerRunningImagePathValue = None
 
     def __init__(self, x, y, playerId):
         self.playerId = playerId
         self.x = x
         self.y = y
         self.speed = 3
-        self.state = 0
+        self.stateFrame = 0
 
-    def getImagePath(self, direction, moveState, frame=None):
+    def getImagePath(self, direction: Direction, moveState: MoveState, frame=None):
         relativePath = "assets/player/char" + str(self.playerId) + "_"
-        framePath = ""
+        runningImagePathValue = ""
         if frame:
-            framePath = "_" + str(frame)
-        return relativePath + direction.value + "_" + moveState.value + framePath + ".png"
+            runningImagePathValue = "_" + str(frame)
+
+        if direction is None or moveState is None:
+            return relativePath + "front_standing.png"
+        else:
+            return relativePath + direction.value + "_" + moveState.value + runningImagePathValue + ".png"
 
     def getImageFromPath(self, path):
         return pygame.image.load(path)
 
     def draw(self, win):
-        imagePath = self.getImagePath(self.playerDirection, self.playerMoveState, self.playerMoveFrame)
+        imagePath = self.getImagePath(self.playerDirection, self.playerMoveState, self.playerRunningImagePathValue)
         img = self.getImageFromPath(imagePath)
         win.blit(img, (self.x, self.y))
+
+
+    def setMoveState(self):
+        if 10 <= self.stateFrame < 20 or 30 <= self.stateFrame < 40:
+            self.playerMoveState = MoveState.RUNNING
+            if self.playerDirection == Direction.UP or self.playerDirection == Direction.DOWN:
+                self.playerRunningImagePathValue = 1 if 10 <= self.stateFrame < 20 else 2
+            else:
+                self.playerRunningImagePathValue = None
+        else:
+            self.playerMoveState = MoveState.STANDING
+            self.playerRunningImagePathValue = None
+            if self.stateFrame >= 40:
+                self.stateFrame = 0
 
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
+            self.stateFrame += 1
             self.x -= self.speed
             self.playerDirection = Direction.LEFT
-            if self.state >= 0 and self.state < 10:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.playerMoveState = MoveState.RUNNING
-                self.playerMoveFrame = None
-                self.state += 1
-            else:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state = 0
-
         elif keys[pygame.K_RIGHT]:
+            self.stateFrame += 1
             self.x += self.speed
             self.playerDirection = Direction.RIGHT
-            if self.state >= 0 and self.state < 10:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.playerMoveState = MoveState.RUNNING
-                self.playerMoveFrame = None
-                self.state += 1
-            else:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state = 0
-
         if keys[pygame.K_UP]:
+            self.stateFrame += 1
             self.y -= self.speed
             self.playerDirection = Direction.UP
-            if self.state >= 0 and self.state < 10:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.playerMoveState = MoveState.RUNNING
-                self.playerMoveFrame = 1
-                self.state += 1
-            elif self.state >= 20 and self.state < 30:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state += 1
-            elif self.state >= 30 and self.state < 40:
-                self.playerMoveState = MoveState.RUNNING
-                self.playerMoveFrame = 2
-                self.state += 1
-            elif self.state >= 40:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state = 0
-
         elif keys[pygame.K_DOWN]:
+            self.stateFrame += 1
             self.y += self.speed
             self.playerDirection = Direction.DOWN
-            if self.state >= 0 and self.state < 10:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.playerMoveState = MoveState.RUNNING
-                self.playerMoveFrame = 1
-                self.state += 1
-            elif self.state >= 20 and self.state < 30:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state += 1
-            elif self.state >= 30 and self.state < 40:
-                self.playerMoveState = MoveState.RUNNING
-                self.playerMoveFrame = 2
-                self.state += 1
-            elif self.state >= 40:
-                self.playerMoveState = MoveState.STANDING
-                self.playerMoveFrame = None
-                self.state = 0
+        self.setMoveState()
