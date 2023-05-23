d711843db21c951941787d6283468e4a151a59d7
Olek Golus <84678175+olekgolus11@users.noreply.github.com>
2023-05-23 14:19:29 +0200

    Refactor player (#10)
    * Extract image path
    
    * Change move function
    
    * Clear move function, extract to seperate functions
    
    Clear more code
    
    * Add string literals, increase readability
    
    * Extract checking players state
    
    * Update gitignore
    
    * Delete Map/__pycache__ directory
    
    * Rename constants typo
    
    * Extract more helper functions

--- Player.py
+++ Player.py
@@ -1,104 +1,90 @@
 import pygame
+from enum import Enum
+import constants
+
+
+class Direction(Enum):
+    UP = 'back'
+    DOWN = 'front'
+    LEFT = 'left'
+    RIGHT = 'right'
+
+
+class MoveState(Enum):
+    STANDING = 'standing'
+    RUNNING = 'running'
 
 
 class Player:
-    CHAR_FRONT_STANDING = None
-    CHAR_FRONT_RUNNING_1 = None
-    CHAR_FRONT_RUNNING_2 = None
+    PLAYER_ANIMATION_SPEED_MULTIPLIER = 0.7
 
-    CHAR_BACK_STANDING = None
-    CHAR_BACK_RUNNING_1 = None
-    CHAR_BACK_RUNNING_2 = None
-
-    CHAR_LEFT_STANDING = None
-    CHAR_LEFT_RUNNING = None
-
-    CHAR_RIGHT_STANDING = None
-    CHAR_RIGHT_RUNNING = None
+    playerDirection = Direction.DOWN
+    playerMoveState = MoveState.STANDING
+    playerRunningImagePathValue = None
 
     def __init__(self, x, y, playerId):
         self.playerId = playerId
         self.x = x
         self.y = y
         self.speed = 3
-        self.state = 0
-        self.createImages()
-        self.characterImg = self.CHAR_FRONT_STANDING
-
-    def createImages(self):
-        self.CHAR_FRONT_STANDING = "assets/player/char" + str(self.playerId) + "_front_standing.png"
-        self.CHAR_FRONT_RUNNING_1 = "assets/player/char" + str(self.playerId) + "_front_running_1.png"
-        self.CHAR_FRONT_RUNNING_2 = "assets/player/char" + str(self.playerId) + "_front_running_2.png"
-
-        self.CHAR_BACK_STANDING = "assets/player/char" + str(self.playerId) + "_back_standing.png"
-        self.CHAR_BACK_RUNNING_1 = "assets/player/char" + str(self.playerId) + "_back_running_1.png"
-        self.CHAR_BACK_RUNNING_2 = "assets/player/char" + str(self.playerId) + "_back_running_2.png"
-
-        self.CHAR_LEFT_STANDING = "assets/player/char" + str(self.playerId) + "_left_standing.png"
-        self.CHAR_LEFT_RUNNING = "assets/player/char" + str(self.playerId) + "_left_running.png"
-
-        self.CHAR_RIGHT_STANDING = "assets/player/char" + str(self.playerId) + "_right_standing.png"
-        self.CHAR_RIGHT_RUNNING = "assets/player/char" + str(self.playerId) + "_right_running.png"
-
-    def draw(self, win):
-        img = pygame.image.load(self.characterImg)
-        win.blit(img, (self.x, self.y))
+        self.frameState = 0
+        self.fullMoveTimeframe = constants.FPS * self.PLAYER_ANIMATION_SPEED_MULTIPLIER
 
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
+            self.frameState += 1
             self.x -= self.speed
-            if self.state >= 0 and self.state < 10:
-                self.characterImg = self.CHAR_LEFT_STANDING
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.characterImg = self.CHAR_LEFT_RUNNING
-                self.state += 1
-            else:
-                self.state = 0
-
+            self.playerDirection = Direction.LEFT
         elif keys[pygame.K_RIGHT]:
+            self.frameState += 1
             self.x += self.speed
-            if self.state >= 0 and self.state < 10:
-                self.characterImg = self.CHAR_RIGHT_STANDING
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.characterImg = self.CHAR_RIGHT_RUNNING
-                self.state += 1
-            else:
-                self.state = 0
-
+            self.playerDirection = Direction.RIGHT
         if keys[pygame.K_UP]:
+            self.frameState += 1
             self.y -= self.speed
-            if self.state >= 0 and self.state < 10:
-                self.characterImg = self.CHAR_BACK_STANDING
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.characterImg = self.CHAR_BACK_RUNNING_1
-                self.state += 1
-            elif self.state >= 20 and self.state < 30:
-                self.characterImg = self.CHAR_BACK_STANDING
-                self.state += 1
-            elif self.state >= 30 and self.state < 40:
-                self.characterImg = self.CHAR_BACK_RUNNING_2
-                self.state += 1
-            elif self.state >= 40:
-                self.state = 0
-
+            self.playerDirection = Direction.UP
         elif keys[pygame.K_DOWN]:
+            self.frameState += 1
             self.y += self.speed
-            if self.state >= 0 and self.state < 10:
-                self.characterImg = self.CHAR_FRONT_STANDING
-                self.state += 1
-            elif self.state >= 10 and self.state < 20:
-                self.characterImg = self.CHAR_FRONT_RUNNING_1
-                self.state += 1
-            elif self.state >= 20 and self.state < 30:
-                self.characterImg = self.CHAR_FRONT_STANDING
-                self.state += 1
-            elif self.state >= 30 and self.state < 40:
-                self.characterImg = self.CHAR_FRONT_RUNNING_2
-                self.state += 1
-            elif self.state >= 40:
-                self.state = 0
+            self.playerDirection = Direction.DOWN
+        self.updatePlayerMoveState()
 
+    def draw(self, win):
+        image = self.getPlayerImage()
+        win.blit(image, (self.x, self.y))
+
+    def getPlayerImage(self):
+        return pygame.image.load(self.getImagePath())
+
+    def getImagePath(self):
+        relativePath = f"assets/player/char{self.playerId}_"
+        runningImagePathValue = f"_{self.playerRunningImagePathValue}" if self.playerRunningImagePathValue else ""
+        if self.playerDirection is None or self.playerMoveState is None:
+            return f"{relativePath}front_standing.png"
+        else:
+            return f"{relativePath}{self.playerDirection.value}_{self.playerMoveState.value}{runningImagePathValue}.png"
+
+    def updatePlayerMoveState(self):
+        if self.shouldPlayerBeInRunningState():
+            self.playerMoveState = MoveState.RUNNING
+            self.updatePlayerRunningImagePathValue()
+        else:
+            self.playerMoveState = MoveState.STANDING
+            self.playerRunningImagePathValue = None
+            if self.frameState >= self.fullMoveTimeframe:
+                self.frameState = 0
+
+    def shouldPlayerBeInRunningState(self):
+        frame = self.fullMoveTimeframe
+        if frame * 0.25 <= self.frameState < frame * 0.5 or frame * 0.75 <= self.frameState < frame:
+            return True
+        else:
+            return False
+
+    def updatePlayerRunningImagePathValue(self):
+        frame = self.fullMoveTimeframe
+        if self.playerDirection == Direction.UP or self.playerDirection == Direction.DOWN:
+            self.playerRunningImagePathValue = 1 if frame * 0.25 <= self.frameState < frame * 0.5 else 2
+        else:
+            self.playerRunningImagePathValue = None
